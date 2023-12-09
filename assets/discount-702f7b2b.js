d7c153840f9b0476bcd31e9c31401ac5b8b9cdde
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2023-12-09 18:44:06 +0000

    Deploying to gh-pages from @ ValentynHromadskyi/Project-JS-The-Code-Magic@3485ac99bf3ac2bb92127e26d621447ff9a7cb9b 🚀

--- assets/discount-702f7b2b.js
+++ assets/discount-702f7b2b.js
@@ -40,4 +40,4 @@ import{a}from"./vendor-a61d8330.js";(function(){const o=document.createElement("
         </div>
       </div>
     `}async function F(){try{const t=await l(),o=g(t,2),r=document.getElementById("discountContainer"),c=document.createDocumentFragment();o.forEach(e=>{c.appendChild(f(e))}),r.appendChild(c)}catch(t){console.error(t.message)}}F();l().then(t=>t.data).then(t=>{const o=g(t,2),r=document.getElementById("discountContainer");o.forEach(c=>{r.innerHTML+=f(c)})}).catch(t=>{console.error(t.message)});
-//# sourceMappingURL=discount-1cb9a112.js.map
+//# sourceMappingURL=discount-702f7b2b.js.map
