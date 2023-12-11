e5af819b8341f55184460efe3fe837963e4d6f8a
Katya <drozdkatya114@gmail.com>
2023-12-11 04:28:33 +0200

    filtration

--- src/js/prodacts-list.js
+++ src/js/prodacts-list.js
@@ -1,5 +1,9 @@
-// функція для отримання продуктів з сервера
+import { getFilteredProduct } from "./api";
 
+
+
+
+// функція для отримання продуктів з сервера
 async function getProductsList(keyword, category, page = 1, limit = 6) {
   const url = `https://food-boutique.b.goit.study/api/products?keyword=${keyword}&category=${category}&page=${page}&limit=${limit}`;
 
@@ -29,33 +33,34 @@ function getLimit() {
 
 //картка продукта
 
-function renderProductCard(product) {
+
+function renderProductCard(data) {
   return `
-    <div class="productlist-card" data-productlist-id="${product._id}">
+    <div class="productlist-card" data-productlist-id="${data._id}">
       <div class="productlist-background">
-        <img src="${product.img}" alt="${product.name}" class="product-image">
+        <img src="${data.img}" alt="${data.name}" class="product-image">
       </div>
       <div class="productlist-details">
         <div class="productlist-details-text">
-          <h2 class="productlist-name">${product.name}</h2>
+          <h2 class="productlist-name">${data.name}</h2>
           <div class="pl-det">
           
           <div class="category-cont">
           <p class="productlist-category">Category:
-          <span class="word">${product.category}</span></p>
+          <span class="word">${data.category}</span></p>
 
           <p class="productlist-size">Size:
-          <span class="word">${product.size}</span></p>
+          <span class="word">${data.size}</span></p>
           </div>
 
           <div class="popularity-cont">
           <p class="productlist-popularity">Popularity:
-          <span class="word">${product.popularity}</span></p>
+          <span class="word">${data.popularity}</span></p>
           </div>
 
           </div>
           <div class="price-icon">
-          <p class="productlist-price">$${product.price.toFixed(2)}</p>
+          <p class="productlist-price">$${data.price.toFixed(2)}</p>
           <div class="price-icon-cont">
           <svg class="productlist-icon" width="18" height="18">
             <use href="../icons.svg#icon-shopping-cart"></use>
@@ -67,25 +72,47 @@ function renderProductCard(product) {
     </div>
   `;
 }
+  
+const refs = {
+  filterCatList: document.querySelector('.filters-categories'),
+  form: document.getElementById('fiters-form'),
+  filtersInput: document.querySelector('.filters-input'),
+  filtersCategories: document.querySelector('.filters-categories'),
+};
 
-import { getFilteredProduct } from './api.js';
+refs.filterCatList.addEventListener("change", changeFilter)
 
-const filters = JSON.parse(localStorage.getItem('filters'));
-console.log(filters);
 
-// Використовуйте async/await для чистішого синтаксису
-async function fetchData() {
+async function changeFilter(event) {
+  let category = event.target.value;
+  const filters = {
+    category: category,
+    byABC: true,
+    byPrice: false,
+    byPopularity: false,
+    page: 1,
+    limit: 6,
+  };
+
   try {
-    const product = await getFilteredProduct(filters);
-    console.log(product);
+    const result = await getFilteredProduct(filters);
+    const data = result.results
+
+    console.log(data);
+
+    const productList = document.getElementById('productList');
+    productList.innerHTML = '';
+
+    data.forEach(product => {
+      productList.innerHTML += renderProductCard(product);
+    });
+
+    const totalPages = Math.ceil(result.total / filters.limit);
+    renderPagination(totalPages, filters.page);
   } catch (error) {
-    console.log('error', error);
+    console.error('Произошла ошибка:', error);
   }
 }
-
-// Викликайте функцію fetchData
-fetchData();
-  
   
 async function fetchAndRenderProducts(page = 1) {
   const keyword = '';
