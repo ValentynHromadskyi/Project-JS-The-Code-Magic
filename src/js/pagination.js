1eff3fb9e459cd9876f2de6a5feb4c1e35e87c52
Katya <drozdkatya114@gmail.com>
2023-12-13 21:04:17 +0200

    try45

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,35 +1,128 @@
-const PAGE_SIZE = 6;
+// const PAGE_SIZE = 6;
+// let currentPage = 1;
+
+// function renderPagination(totalPages, currentPage) {
+//   const paginationCont = document.getElementById('btn-numbers');
+//   paginationCont.innerHTML = ''; // Очистка содержимого контейнера перед добавлением новых элементов
+
+//   for (let i = 1; i <= totalPages; i++) {
+//     const pageNumber = document.createElement('span');
+//     pageNumber.classList.add('page-number');
+
+    // if (i === 1 && currentPage > 1) {
+    //   pageNumber.textContent = '...';
+    //   pageNumber.classList.add('dots');
+    // } else if (i === currentPage) {
+    //   pageNumber.textContent = currentPage;
+    //   pageNumber.classList.add('active');
+    // } else if (i === currentPage + 1 && currentPage < totalPages) {
+    //   pageNumber.textContent = currentPage + 1;
+    // } else if (i === currentPage - 1 && currentPage > 1) {
+    //   pageNumber.textContent = currentPage - 1;
+    // } else {
+    //   pageNumber.textContent = i;
+    // }
+
+//     paginationCont.appendChild(pageNumber);
+
+//     // Добавляем обработчик события click для каждого элемента pageNumber
+//     if (i === 1 || i === totalPages) {
+//       pageNumber.addEventListener('click', function () {
+//         changePage(i);
+//       });
+//     }
+//   }
+
+//   document.getElementById('btn-left').addEventListener('click', function () {
+//     if (currentPage > 1) {
+//       changePage(currentPage - 1);
+//     }
+//   });
+
+//   document.getElementById('btn-right').addEventListener('click', function () {
+//     if (currentPage < totalPages) {
+//       changePage(currentPage + 1);
+//     }
+//   });
+// }
+
+
+// const PAGE_SIZE = totalPages;
+
 
 function renderPagination(totalPages, currentPage) {
-  
+
   const paginationCont = document.getElementById('btn-numbers');
   paginationCont.innerHTML = ''; // Очистка содержимого контейнера перед добавлением новых элементов
 
-  
+  for (let i = 1; i <= totalPages; i++) {
+    // function renderNumberCard(number) {
+    //   return `
+    // <div id="btn-numbers" class="btn-numbers"><span>${number}</span></div>`
+    // }
 
-  for (let i = 1; i <= 6; i++) {
-  const pageNumber = document.createElement("span");
-  pageNumber.classList.add("page-number");
-  
-  if (i <= 4) {
+    const pageNumber = document.createElement("span");
+    pageNumber.classList.add("page-number");
+    let pageNumber = i;
     pageNumber.textContent = i;
-  } else if (i === 5) {
-    pageNumber.textContent = "...";
-    pageNumber.classList.add("dots");
-  } else {
-    pageNumber.textContent = 6;
-  }
-  
-  paginationCont.appendChild(pageNumber);
-  
-  // Добавляем обработчик события click для каждого элемента pageNumber
-  if (i <= 4 || i === 6) {
+    paginationCont.appendChild(pageNumber);
+    
+    Добавляем обработчик события click для каждого элемента pageNumber
     pageNumber.addEventListener('click', function() {
       changePage(i);
     });
-    }
+
+    renderNumberCard(i);
   }
-}
+
+  // for (let i = 1; i <= totalPages; i++) {
+  // const pageNumber = document.createElement("span");
+  // pageNumber.classList.add("page-number");
+  
+    // if (i === 1 && currentPage > 4) {
+    //   pageNumber.textContent = '...';
+    //   pageNumber.classList.add('dots');
+    // } else if (i === currentPage) {
+    //   pageNumber.textContent = currentPage;
+    //   pageNumber.classList.add('active');
+    // } else if (i === currentPage + 1 && currentPage < totalPages) {
+    //   pageNumber.textContent = currentPage + 1;
+    // } else if (i === currentPage - 1 && currentPage > 1) {
+    //   pageNumber.textContent = currentPage - 1;
+    // } else {
+    //   pageNumber.textContent = i;
+    // }
+    
+
+  // for (let i = 1; i <= totalPages; i++) {
+  //   const pageNumber = document.createElement("span");
+  //   pageNumber.classList.add("page-number");
+  //   pageNumber.textContent = i;
+
+  //   paginationCont.appendChild(pageNumber);
+    
+  //   pageNumber.addEventListener('click', )
+  // }
+  
+  // if (i <= 4) {
+  //   pageNumber.textContent = i;
+  // } else if (i === 5) {
+  //   pageNumber.textContent = "...";
+  //   pageNumber.classList.add("dots");
+  // } else {
+  //   pageNumber.textContent = i;
+  // }
+  
+ 
+
+  
+  // Добавляем обработчик события click для каждого элемента pageNumber
+  // if (i <= 4 || i === totalPages) {
+  //   pageNumber.addEventListener('click', function() {
+  //     changePage(i);
+  //   });
+  //   }
+  }
 
 
 
