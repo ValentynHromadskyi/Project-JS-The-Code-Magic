4c64404ba0b0553ae64dd0591be2a75363ceec12
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-12-14 18:31:26 +0200

    footer commit

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,95 +1,78 @@
 import { postSubscription } from "./api";
 
-const footerForm = document.querySelector('.foot-form');
+
+const formElem = document.querySelector('.form-footer');
 const modalSubscription = document.querySelector('.modal-subscription');
 const modalUnsubscription = document.querySelector('.modal-unsubscription');
-const modalBackdrop = document.querySelector('.modal-backdrop');
-const body = document.querySelector('.body');
-const overflow = document.body.style.overflow;
+const modalBackElem = document.querySelector('.modal-backdrop-subscription');
+const closeBackDrop = document.querySelector('.container-modal');
+modalSubscription.classList.add('is-hidden');
+modalUnsubscription.classList.add('is-hidden');
 
 const closeModal = event => {
-  const target = event.target;
-  if (target === modalSubscription || target.closest('.close')) {
-    modalSubscription.classList.remove('visible-modal');
-    setTimeout(() => modalSubscription.classList.add('visually-hidden'), 500);
-    modalBackdrop.classList.add('is-hidden');
-
-    document.removeEventListener('keydown', closeModal);
-    body.style.overflow = overflow;
-  }
-  if (target === modalUnsubscription || target.closest('.close')) {
-    modalUnsubscription.classList.remove('visible-modal');
-    setTimeout(() => modalUnsubscription.classList.add('visually-hidden'), 500);
-    modalBackdrop.classList.add('is-hidden');
-
-    document.removeEventListener('keydown', closeModal);
-    body.style.overflow = overflow;
-  }
-
-  if (event.key === 'Escape') {
-    closeModalOption();
-  }
-
-  if (target === modalBackdrop) {
-    closeModalOption();
-  }
+ const target = event.target;
+ if (target === modalSubscription || target.closest('.close')) {
+  modalSubscription.classList.add('is-hidden');
+  modalBackElem.classList.add('is-hidden');
+ }
+ if (target === modalUnsubscription || target.closest('.close')) {
+  modalUnsubscription.classList.add('is-hidden');
+  modalBackElem.classList.add('is-hidden');
+ }
+ if (target === closeBackDrop || target.closest('.close')) {
+  modalUnsubscription.classList.add('is-hidden');
+  modalBackElem.classList.add('is-hidden');
+ }
 };
 
-const openModalSub = () => {
-    modalUnsubscription.classList.add('visible-modal');
-    modalUnsubscription.classList.remove('visually-hidden');
-    modalBackdrop.classList.remove('is-hidden');
-    
-    document.addEventListener('keydown', closeModal);
-    body.style.overflow = 'hidden';
+function onCloseModal(evt) {
+ if (evt.key === 'Escape' || evt.target === modalBackElem) {
+  modalSubscription.classList.add('is-hidden');
+  modalUnsubscription.classList.add('is-hidden');
+  modalBackElem.classList.add('is-hidden');
+  document.body.style.overflow = 'auto';
+ }
+}
+
+const openModalSubscription = () => {
+ modalSubscription.classList.remove('is-hidden');
+ modalBackElem.classList.remove('is-hidden');
+ document.body.style.overflow = 'hidden';
 };
 
 const openModalUnsubscription = () => {
-    modalUnsubscription.classList.add('visible-modal');
-    modalUnsubscription.classList.remove('visually-hidden');
-    modalUnsubscription.classList.remove('is-hidden');
-    
-    document.addEventListener('keydown', closeModal);
-    body.style.overflow = 'hidden';
+ modalUnsubscription.classList.remove('is-hidden');
+ modalBackElem.classList.remove('is-hidden');
+ document.body.style.overflow = 'hidden';
 };
 
-
 const handleSubscription = async email => {
-    const body = { email };
+ const body = { email };
 
-    try {
-        const emailToAdd = await postSubscription(body);
+ try {
+  const checkedEmail = await postSubscription(body);
 
-        openModalSub();
-        footerForm.reset();
-    } catch (err) {
-        if (err.response.data.message === 'Subscription already exists') {
-            openModalUnsubscription();
-        } else {
-            showError();
-        }
-    }
+  openModalSubscription();
+ } catch (err) {
+  if (err.response.data.message === 'Subscription already exists') {
+   openModalUnsubscription();
+  } else {
+   showError();
+  }
+ }
 };
 
 function handleSubmit(e) {
-    e.preventDefault();
-    const inputEmail = document.querySelector('.form-filed');
-    const email = inputEmail.ariaValueMax.trim();
-    handleSubscription(email);
-} 
-
-function closeModalOption() {
-    modalSubscription.classList.remove('visible-modal');
-    setTimeout(() => modalSubscription.classList.add('visually-hidden'), 500);
-    modalBackdrop.classList.add('is-hidden');
-
-    modalUnsubscription.classList.remove('visible-modal');
-    setTimeout(() => modalUnsubscription.classList.add('visually-hidden'), 500);
-    modalBackdrop.classList.add('is-hidden');
-
-    document.removeEventListener('keydown', closeModal);
-    body.style.overflow = overflow;
-
-    formElem.addEventListener('submit', handleSubmit);
-    modalBackdrop.addEventListener('click', closeModal);
+ e.preventDefault();
+ const emailInput = document.querySelector('.input-label');
+ const email = emailInput.value.trim();
+ handleSubscription(email);
+ formElem.reset();
 }
+
+formElem.addEventListener('submit', handleSubmit);
+modalSubscription.addEventListener('click', closeModal);
+modalUnsubscription.addEventListener('click', closeModal);
+modalBackElem.addEventListener('click', closeModal);
+document.addEventListener('keydown', onCloseModal);
+document.addEventListener('click', onCloseModal);
